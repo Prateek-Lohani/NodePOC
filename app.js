90c41dbca69410a9ea0427b8a5a23eee4f2b17c4
PrateekLohani <prateeklohani105@gmail.com>
2025-07-26 18:15:59 +0530

    Using JSON file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const {sum , multiply} = require('./calculate')
 
+const data = require('./data.json')
+
 console.log('app')
+console.log(data.map((item)=>item.title))
+
 
 sum(5,15)
 multiply(5,15)
