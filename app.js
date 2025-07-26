d5d1a3134997cbc2faa04bd1093b1b3edce79014
PrateekLohani <prateeklohani105@gmail.com>
2025-07-26 18:31:21 +0530

    Inbuilt node.js module (util module)

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const {sum , multiply} = require('./calculate')
 
 const data = require('./data.json')
+const util= require('node:util')
 
 console.log('app')
 console.log(data.map((item)=>item.title))
-
+console.log(util)
 
 sum(5,15)
 multiply(5,15)
