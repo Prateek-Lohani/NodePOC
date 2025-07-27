f9862da4b5937deb13707cbe21d81435009de65f
PrateekLohani <prateeklohani105@gmail.com>
2025-07-27 12:46:36 +0530

    How to create a hased password to securely save it in database

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const crypto=require('node:crypto')
-const data='This will be encrypted by crypto'
 
-const md5=crypto.createHash('md5').update(data).digest('hex') // not recommended as cryptographically weak
-console.log('MD5:',md5)
+const password='SomeSecurePassword';
+const salt=crypto.randomBytes(16).toString('hex');  // adding random string that is unique to each user
+const iterations=1000000; // It makes the hash slower to compute,  More iterations = better security but But too many = slow login performance
+const keylen=64;  // this sets the length (in bytes) of the final derived key (the hashed password) [64 bytes=128 hex characters].
+const digest='sha512'; // specifies hash function
 
-const sha1=crypto.createHash('sha1').update(data).digest('hex') // not recommended as cryptographically weak
-console.log('SHA1:',sha1)
+crypto.pbkdf2(password,salt,iterations,keylen,digest,(err,derivedKey)=>{
+    if(err) throw err;
 
-const sha256=crypto.createHash('sha256').update(data).digest('hex') 
-console.log('SHA256:',sha256)
+    console.log('User Password:',password)
+    console.log('Salt:',salt);
+    console.log('Hashed Password:', derivedKey.toString('hex')); // this is the hashed password that you can store in your database
+})
 
-const sha512=crypto.createHash('sha512').update(data).digest('hex') 
-console.log('SHA512:',sha512)
